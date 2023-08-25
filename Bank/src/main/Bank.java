7bd505156ce24b501541ff621748fab1cafbb0fb
Erick Dias <96884535+erickbeavis@users.noreply.github.com>
2023-08-25 20:36:42 -0300

    Add files via upload
    Criando swich case para dar opções ao usuário caso ele desejar realizar somente uma das operações.
    
    //ainda em construção

--- Bank/src/main/Bank.java
+++ Bank/src/main/Bank.java
@@ -1,17 +1,20 @@
-package main;
+﻿package main;
 
+import main.Account;
 import java.util.Scanner;
 
 public class Bank {
 
 	public static void main(String[] args) {
 		
+		boolean isRunning = true;
+		
 		Scanner sc = new Scanner (System.in);
-
+		
 		System.out.println("Bem vindo ao Banco do Beavis!");
 		System.out.println();
 		
-		Account account = new Account ();
+		Account account = new Account();
 		
 		System.out.println("Para criar sua conta por favor digite o número da conta que deseja criar: ");
 		
@@ -29,38 +32,54 @@ public class Bank {
 		
 		System.out.println("Parabéns, você criou sua conta!");
 		System.out.println("Número da conta: " + account.accountnumber + ", Nome: " + account.name + ", Saldo total: R$ " + account.saldo);
-		
-		System.out.println();
-		
-		System.out.println("Digite o valor que deseja depositar: ");
-		
-		double valor = sc.nextDouble();
-		
-		account.Depositar(valor);
 
+		
 		System.out.println();
-		System.out.println("O saldo total da sua conta ficou: " + account.saldo);
-		System.out.println();
 		
-		//FINAL DEPOSITAR
-		
-		System.out.println("Digite o valor que deseja sacar: ");
-		
-		double valor1 = sc.nextDouble();
-		
-		account.Sacar(valor1);
-		
+		System.out.println("Digite: 1. Depositar");
+		System.out.println("Digite: 2. Sacar");
+		System.out.println("Digite: 3. Consulta");
 
-		System.out.println();
-		System.out.println("O saldo total da sua conta ficou: " + account.saldo);
 		
-		System.out.println();
-		System.out.println("Número da conta: " + account.accountnumber + ", Nome: " + account.name + ", Saldo total: R$ " + account.saldo);
-		System.out.println();
+		int choice = sc.nextInt();
+
+		while (isRunning){
+		
+		switch (choice){
+		
+		case 1 :
+			System.out.println("Digite o valor que deseja depositar: ");
+			
+			double valor = sc.nextDouble();
+			
+			account.Depositar(valor);
+			
+			break;
+			
+		case 2 :
+			
+			System.out.println("Digite o valor que deseja sacar: ");
+			
+			double valor1 = sc.nextDouble();
+			
+			account.Sacar(valor1);
+			
+			break;
+			
+		case 3:
+			
+			System.out.println();
+			System.out.println("Número da conta: " + account.accountnumber + ", Nome: " + account.name + ", Saldo total: R$ " + account.saldo);
+			System.out.println();
+			
+			
+		}
+		
+	}
+		sc.close();
 		
 		System.out.println("Obrigado, finalizando sessão... [\\\\\\\\\\\\XXXXXXXX]");
 		
-		sc.close();
+		}
+		
 	}
-
-}
